fc022141f39e63b47c77425c542c81e57489e3af
wj <810068252@qq.com>
2019-09-01 19:32:29 +0800

    新建登录主页

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'element-ui/lib/theme-chalk/index.css'
+import './style/index.less'
 
 Vue.config.productionTip = false
 
