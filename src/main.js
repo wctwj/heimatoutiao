f5b782ac9beced525421bc7f27fa7079ab089a45
wj <810068252@qq.com>
2019-09-01 23:31:28 +0800

    安装axios配置全局使用

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import ElementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './style/index.less'
+import axios from 'axios'
+Vue.prototype.$axios = 'axios'
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 
 Vue.config.productionTip = false
 Vue.use(ElementUi)
