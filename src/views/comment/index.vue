2dae34dea4a022fbc1966a46eee4f0e5ceda09b1
wj <810068252@qq.com>
2019-09-10 20:49:42 +0800

    评论列表加载状态

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-card>
+  <el-card v-loading='loading'>
     <bread-crumb slot="header">
       <template slot="title">
         评论列表
@@ -35,6 +35,7 @@
 export default {
   data(){
     return{
+      loading:false,
       list:[],
       page:{
         page:1,
@@ -66,10 +67,12 @@ export default {
       return row.comment_status ? '正常' : '关闭'
     },
     getComments(){
+      this.loading = true
       this.$axios({
         url:'/articles',
         params:{response_type:'comment',page:this.page.page,per_page:this.page.pageSize}
       }).then(result=>{
+        this.loading = false
         this.list = result.data.results
         this.page.total = result.data.total_count
       })
