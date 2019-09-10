497c713e924ab735b5ba8a0b436e126ca4df8a90
wj <810068252@qq.com>
2019-09-10 21:58:21 +0800

    素材列表数据

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,24 +1,85 @@
 <template>
-  <el-card>
+  <el-card class="material">
     <bread-crumb slot="header">
       <template slot="title">素材管理</template>
     </bread-crumb>
-    <el-tabs v-model="activeName" @tab-click="handleClick">
-    <el-tab-pane label="全部素材" name="all">全部</el-tab-pane>
-    <el-tab-pane label="收藏图片" name="collect">收藏</el-tab-pane>
-  </el-tabs>
+    <el-tabs v-model="activeName" @tab-click="changeTab">
+      <el-tab-pane label="全部素材" name="all">
+        <div class="card-list">
+          <el-card v-for="item in list" :key="item.id" class="img-card">
+            <img :src="item.url" alt />
+            <el-row type="flex" justify="space-around" class="operate" align="middle">
+              <i :style="{color:item.is_collected ? 'red' : ''}" class="el-icon-star-on"></i>
+              <i class="el-icon-delete-solid"></i>
+            </el-row>
+          </el-card>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="收藏图片" name="collect">
+        <div class="card-list">
+          <el-card v-for="item in list" :key="item.id" class="img-card">
+            <img :src="item.url" alt />
+          </el-card>
+        </div>
+      </el-tab-pane>
+    </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            activeName:'all'
+  data() {
+    return {
+      activeName: "all",
+      list: []
+    };
+  },
+  methods: {
+      changeTab(){
+          this.getMaterial()
+      },
+    getMaterial() {
+      this.$axios({
+        url: "/user/images",
+        params: {
+          collect: this.activeName === 'collect'
         }
+      }).then(result => {
+        this.list = result.data.results;
+      });
     }
+  },
+  created() {
+    this.getMaterial();
+  }
 };
 </script>
 
-<style>
+<style lang='less' scoped>
+.material {
+  .card-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    .img-card {
+      width: 180px;
+      height: 180px;
+      margin: 30px;
+      position: relative;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+      .operate {
+        position: absolute;
+        width: 100%;
+        bottom: 0;
+        left: 0;
+        height: 30px;
+        font-size: 20px;
+        background-color: #ccc;
+      }
+    }
+  }
+}
 </style>
